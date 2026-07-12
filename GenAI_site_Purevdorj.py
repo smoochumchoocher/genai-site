5104a24632485d8b63a04f377e311415cbc7e39a
Purevdorj <uyaruun@gmail.com>
2026-07-12 16:15:37 -0500

    Update GenAI_site_Purevdorj.py

--- GenAI_site_Purevdorj.py
+++ GenAI_site_Purevdorj.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_openai import OpenAI
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
